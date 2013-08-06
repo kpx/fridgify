de2a747d822ab8663898e2ef62b4122f0b4fbd9d
Jonas Hadin <jonas.hadin@seal-software.com>
2013-08-06 15:14:14 +0200

    fixed uri storing and retrieving

--- js/fridge.js
+++ js/fridge.js
@@ -1,9 +1,24 @@
 var Fridge = (function (fridge) {
     
-	fridge.add_magnet = function(name) {
-		var magnet = '<li>' + name + '</li>';
-		$( "#sortable2" ).append(magnet);
+	fridge.addMagnet = function(name, uri) {
+		var magnet = '<li data-uri="' + uri + '">' + name + '</li>';
+		$( "#magnetHolder" ).append(magnet);
 	};
+
+	//Gets the uri from poetry in order
+	fridge.getUris = function() {
+		
+		var res = [];
+		var elems = $( "#poetryHolder li" );
+		$.each(elems, function(e){
+			var uri = $(this).data('uri');
+			res.push(uri);
+		});
+
+
+		return res;
+	};
+
 	return fridge;
 
 } (Fridge || {}));
