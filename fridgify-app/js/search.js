ccd2c44c2cee75c192ea404be672a05260127553
Sebastian Jansson <sebbz@sebbz.se>
2013-08-06 20:26:09 +0200

    Added Spotify app js files

---
+++ fridgify-app/js/search.js
@@ -0,0 +1,33 @@
+var sp = getSpotifyApi();
+var models = sp.require('$api/models');
+
+var SpotSearch = (function (spotsearch) {
+    spotsearch.func = function() {
+    	var searchtext = $( "#searchtext").val();
+        var search = new models.Search(searchtext);
+        search.localResults = models.LOCALSEARCHRESULTS.APPEND;
+
+        var searchHTML = document.getElementById('results');
+        searchHTML.innerHTML = ""
+
+        search.observe(models.EVENT.CHANGE, function() {
+            var results = search.tracks;
+            var fragment = document.createDocumentFragment();
+            for (var i=0; i<results.length; i++){
+                var link = document.createElement('li');
+                var a = document.createElement('a');
+                a.href = results[i].name; 
+                a.href = results[i].uri;
+                link.appendChild(a);
+                a.innerHTML = results[i].name;
+                fragment.appendChild(link);
+            }
+
+            searchHTML.appendChild(fragment);
+        });
+
+        search.appendNext();
+	};
+	return spotsearch;
+
+} (SpotSearch || {}));
